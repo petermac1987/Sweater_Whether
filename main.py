15726cd5c79bc4f97a8df0c8bd03d8353869f129
petermac1987 <petermac@oregonstate.edu>
2019-11-07 17:53:10 -0800

    Modified variables. Removed data sheet.

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 # Author: Chris Peterman
 # Date: 11/01/2019
-# Version: 1.01
+# Version: 2.0
 # Description: This program will
 
 from user_input import Sweater
+
+# Welcome message
+print("Hello, welcome to Sweater Whether.")
+print("This app will take the temperature today and suggest on \'Whether\' you should wear a sweater.")
+
+# User inputs
+print("What is your name? ")
+username = input("> ")
+print("What is the temperature?")
+temperature = int(input("> "))
+
+# Auto output of initial values
+response = Sweater(username, temperature)
+print(response.sweater_whether())
+
+# Change the temperature value in the program.
+while True:
+    print("\nWould you like to change the temperature? y/n")
+    decision = input("> ")
+    if decision.lower() == 'n':
+        print("Good bye!")
+        break
+    elif decision.lower() == 'y':
+        print("What is the new temperature?")
+        temperature = int(input("> "))
+        print(response.sweater_whether())
+    else:
+        print("Invalid input")
--- user_input.py
+++ user_input.py
@@ -4,42 +4,29 @@ class Sweater():
      wear a sweater"""
 
     # Initializes objects of users entered.
-    def __init__(self, username, age, temperature):
+    def __init__(self, username,  temperature):
         """Take in the name of the user and the temperature"""
         self.person_name = username
-        self.person_age = age
         self.temperature_value = temperature
-        self.data = {}
-        self.data['people'] = []
 
-    # Change the value of the temperature, only replaces the value does not add to it.
-    # If possible, look in to API that would give daily weather to populate for user.
-    # Thereby bypassing the need for user to enter data.
     def change_temperature(self, amount):
+        """Changes temperature to value entered"""
         self.temperature_value = amount
 
-    # Current iteration of weather response. You will put in the current temperature and then it will pick a response.
-    # Ultimate goal is having this be individualized bases on when the user picks sweater, jacket, or t-shirt.
-    # Then the user will enter their name and a pin(symbols to prevent use of actual user pins)
-    # This will pull the user data and adjust with user inputs. (Most likely a picture symbol for each type)
-    # Possibly it will check the range of the values stored in each type per individual and check the range of those
-    # values to pick the correct response.
     def sweater_whether(self):
+        """Compares temperature value to set values and returns a response."""
         if self.temperature_value <= 34:
-            print(self.person_name + ", you'll need a heavy jacket. It is " + str(self.temperature_value) +
-                  " Fahrenheit\n")
+            return(self.person_name + ", you'll need a heavy jacket. It is " + str(self.temperature_value) +
+                  " Fahrenheit")
         elif self.temperature_value in range(35, 49):
-            print(self.person_name + ", I would recommend a jacket. It is " + str(self.temperature_value) +
-                  " Fahrenheit\n")
+            return(self.person_name + ", I would recommend a jacket. It is " + str(self.temperature_value) +
+                  " Fahrenheit")
         elif self.temperature_value in range(50, 65):
-            print(self.person_name + ", I would use a sweater. It is " + str(self.temperature_value) +
-                  " Fahrenheit\n")
+            return(self.person_name + ", I would use a sweater. It is " + str(self.temperature_value) +
+                  " Fahrenheit")
         elif self.temperature_value in range(65, 70):
-            print(self.person_name + ", A very light sweater is recommended. It is " + str(self.temperature_value) +
-                  " Fahrenheit\n")
+            return(self.person_name + ", A very light sweater is recommended. It is " + str(self.temperature_value) +
+                  " Fahrenheit")
         else:
-            print(self.person_name + ",  No, stop asking. It is " + str(self.temperature_value) +
-                  " Fahrenheit\n")
-    # I decided to keep this strictly Python for the next few terms. I want this project to showcase my abilities
-    # with this language and then another project for an alternate language. Once I'm more comfortable, I would like
-    # to research other languages to optimize my projects.
+            return(self.person_name + ",  It is way too warm, no sweater unless you hail from the desert. It is " +
+                  str(self.temperature_value) + " Fahrenheit")
